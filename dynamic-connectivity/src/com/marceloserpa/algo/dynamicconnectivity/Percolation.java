96e931a1cc7bd9bc4b430434780e3225862c67ca
marceloserpa <jdev.marcelo@gmail.com>
2020-04-13 10:41:48 -0300

    Starting percolation calc

---
+++ dynamic-connectivity/src/com/marceloserpa/algo/dynamicconnectivity/Percolation.java
@@ -0,0 +1,77 @@
+package com.marceloserpa.algo.dynamicconnectivity;
+
+public class Percolation {
+	
+	private WeightedQuickUnion dynamicConnection;
+	
+	private int lineSize;
+	private boolean[] openSites;
+	
+	private int virtualTop;
+	private int virtualBottom;
+	
+	
+	public Percolation(int n) {
+		lineSize = n;
+		int sites = n * n;
+		dynamicConnection = new WeightedQuickUnion(sites + 2);
+		openSites = new boolean[sites];
+		
+		// creating virtual nodes
+		virtualTop = sites;		
+		virtualBottom = sites + 1;
+		
+		// connect top nodes with head
+		for(int i =0; i < n; i++) {
+			dynamicConnection.union(virtualTop, i);
+		}
+	
+		// connect top nodes with bottom
+		int bottom = (n * n - 1) - n;
+		for(int i = bottom; i < sites; i++) {
+			dynamicConnection.union(virtualBottom, i);
+		}	
+		
+	}
+	
+	public void open(int row, int col) {
+		openSites[(row * lineSize)+ col] = true;
+		
+		// try connect with other 4 neighbors (left, right, up, down) if open
+		//wqu.union(row, col);
+	}
+	
+	public boolean percolate() {
+		return dynamicConnection.connected(virtualTop, virtualBottom);
+	}
+
+	public void render() {
+		System.out.println("Percolation: ");
+		int columnIndex = 1;
+		int[] sites = dynamicConnection.getIds();
+		for(int i = 0; i < sites.length-2; i++) {
+			//System.out.print(" " + ( openSites[i] ? "[" + i + "]" : "(" + i + ")") + " ");
+			System.out.print(" " + ( openSites[i] ? "x" : "-") + " ");
+			if(columnIndex == lineSize) {
+				System.out.println("");	
+				columnIndex = 1;	
+			} else {
+				columnIndex++;
+			}
+		}	
+		
+		System.out.println("Percolate: " + percolate());
+	}	
+	
+	public static void main(String[] args) {
+		Percolation percolation = new Percolation(5);		
+		percolation.open(0, 3);
+		percolation.open(1, 3);
+		percolation.open(2, 3);
+		percolation.open(2, 2);
+		percolation.open(3, 2);
+		percolation.open(4, 2);
+		percolation.render();
+		
+	}
+}
