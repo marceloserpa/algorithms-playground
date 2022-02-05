9af6fa19cf4355ca566978d625a6b2193ddbfbaf
marceloserpa <jdev.marcelo@gmail.com>
2022-02-05 16:10:38 -0300

    insertion sort

---
+++ insertion-sort/src/main/java/Insertion.java
@@ -0,0 +1,25 @@
+public class Insertion {
+
+    public static void sort(Comparable[] comparables){
+        for(int i = 0; i < comparables.length; i++){
+            for(int j = i; j > 0; j--){
+                if(less(comparables[j], comparables[j-1])) {
+                    exchange(comparables, j, j-1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w){
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exchange(Comparable[] comparables, int i,int j){
+        Comparable t = comparables[i];
+        comparables[i] = comparables[j];
+        comparables[j] = t;
+    }
+
+}
