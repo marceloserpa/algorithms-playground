db2c2ecce5c44157b0e3457c668744312f5235a1
marceloserpa <jdev.marcelo@gmail.com>
2022-02-10 21:11:57 -0300

    add merge sort

---
+++ merge-sort/src/main/java/Application.java
@@ -0,0 +1,27 @@
+public class Application {
+    public static void main(String[] args) {
+        Integer[] numbers = new Integer[10];
+        numbers[0] = 9;
+        numbers[1] = 2;
+        numbers[2] = 1;
+        numbers[3] = 3;
+        numbers[4] = 10;
+        numbers[5] = 2;
+        numbers[6] = 6;
+        numbers[7] = 5;
+        numbers[8] = 1;
+        numbers[9] = 0;
+
+        MergeSort.sort(numbers);
+
+        System.out.println("\n\n");
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+
+
+
+
+    }
+
+}
