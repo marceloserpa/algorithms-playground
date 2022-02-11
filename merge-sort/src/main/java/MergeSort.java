bc782447e650d08d3c2923dea22e87af8f60a1e5
marceloserpa <jdev.marcelo@gmail.com>
2022-02-10 21:16:24 -0300

    merge-sort avoid use mergesort for small arrays

--- merge-sort/src/main/java/MergeSort.java
+++ merge-sort/src/main/java/MergeSort.java
@@ -1,5 +1,7 @@
 public class MergeSort {
 
+    private static final int CUTOFF = 5;
+
     public static void sort(Comparable[] a) {
         Comparable[] aux = new Comparable[a.length];
         sort(a, aux, 0, a.length - 1);
@@ -25,7 +27,11 @@ public class MergeSort {
     }
 
     private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
-        if (hi <= lo) return;
+        if (hi <= lo + CUTOFF - 1) {
+            Insertion.sort(a);
+            return ;
+        }
+
         int mid = lo + (hi - lo) / 2;
         sort(a, aux, lo, mid);
         sort(a, aux, mid + 1, hi);
