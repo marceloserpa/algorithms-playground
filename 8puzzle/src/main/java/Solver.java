86f5b0d3813073a3554b54ec062027d77c1fd4af
marceloserpa <jdev.marcelo@gmail.com>
2022-02-21 00:02:29 -0300

    8-puzzle: fixing solver issues

--- 8puzzle/src/main/java/Solver.java
+++ 8puzzle/src/main/java/Solver.java
@@ -7,6 +7,10 @@ public class Solver {
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+
+        if(initial == null) {
+            throw new IllegalArgumentException("error");
+        }
         SearchNode searchNode = new SearchNode(initial, 0, null);
 
         MinPQ<SearchNode> searchNodeMinPQ = new MinPQ<>();
@@ -50,11 +54,14 @@ public class Solver {
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-        return endNode.getNumberOfMoves();
+        return isSolvable() ? endNode.getNumberOfMoves() : -1;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
+        if(!isSolvable()) {
+            return null;
+        }
 
         Stack<Board> stack = new Stack<>();
 
