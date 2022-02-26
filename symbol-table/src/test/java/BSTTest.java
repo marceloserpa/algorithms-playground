9e5cb9d9e06e26c2ae3241d49fbc2b4fa593cc0e
marceloserpa <jdev.marcelo@gmail.com>
2022-02-26 20:35:18 -0300

    symbol table - BST added more test

--- symbol-table/src/test/java/BSTTest.java
+++ symbol-table/src/test/java/BSTTest.java
@@ -17,4 +17,36 @@ class BSTTest {
         Assertions.assertEquals("Joe", playersID.get(33));
     }
 
+    @Test
+    public void testDelete(){
+        BST<Integer, String> playersID = new BST<>();
+
+        playersID.put(18, "Yugi Muto");
+        playersID.put(70, "Bakura");
+        playersID.put(33, "Joe");
+        playersID.put(1, "Marik");
+        playersID.put(0, "Kaiba");
+
+        Assertions.assertEquals("Joe", playersID.get(33));
+
+        playersID.delete(33);
+
+        Assertions.assertNull(playersID.get(33));
+    }
+
+    @Test
+    public void testPutDuplicated(){
+        BST<Integer, String> playersID = new BST<>();
+
+        playersID.put(18, "Yugi Muto");
+        playersID.put(70, "Bakura");
+        playersID.put(33, "Joe");
+        playersID.put(1, "Marik");
+        playersID.put(0, "Kaiba");
+
+        Assertions.assertEquals("Joe", playersID.get(33));
+        playersID.put(33, "Evil Joe");
+        Assertions.assertEquals("Evil Joe", playersID.get(33));
+    }
+
 }
