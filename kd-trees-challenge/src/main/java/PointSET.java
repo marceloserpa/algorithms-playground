550571874ad468020df6d9839496a5ac90a5cb74
marceloserpa <jdev.marcelo@gmail.com>
2022-03-12 17:17:28 -0300

    kd-trees: brute force algo - fixing bugs

--- kd-trees-challenge/src/main/java/PointSET.java
+++ kd-trees-challenge/src/main/java/PointSET.java
@@ -27,27 +27,30 @@ public class PointSET {
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        if (p == null) throw new IllegalArgumentException("invalid argument");
         points.add(p);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
+        if (p == null) throw new IllegalArgumentException("invalid argument");
         return points.contains(p);
     }
 
     // draw all points to standard draw
     public void draw() {
-        for(Point2D point : points) {
+        for (Point2D point : points) {
             point.draw();
         }
     }
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) throw new IllegalArgumentException("invalid argument");
         List<Point2D> pointsInsideBoundary = new ArrayList<>();
 
-        for(Point2D point2D : points) {
-            if(rect.contains(point2D)) {
+        for (Point2D point2D : points) {
+            if (rect.contains(point2D)) {
                 pointsInsideBoundary.add(point2D);
             }
         }
@@ -56,28 +59,18 @@ public class PointSET {
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-
+        if (p == null) throw new IllegalArgumentException("invalid argument");
         Point2D nearest = null;
-        double distance = 0D;
+        double distance = 3D;
 
-        for(Point2D point2D : points) {
-            if(nearest == null) {
+        for (Point2D point2D : points) {
+            if (point2D.compareTo(p) == 0) return point2D;
+            double tmp = p.distanceSquaredTo(point2D);
+            if (distance > tmp) {
+                distance = tmp;
                 nearest = point2D;
-                distance = p.distanceSquaredTo(nearest);
-            } else {
-
-                double tmp = p.distanceSquaredTo(nearest);
-                if(distance > tmp) {
-                    distance = tmp;
-                    nearest = p;
-                }
-
-
             }
-
         }
-
-
         return nearest;
     }
 
@@ -85,18 +78,18 @@ public class PointSET {
     // unit testing of the methods (optional)
     public static void main(String[] args) {
         PointSET pointSET = new PointSET();
-        pointSET.insert(new Point2D(0.2D,0.3D));
-        pointSET.insert(new Point2D(0.1D,0.3D));
-        pointSET.insert(new Point2D(0.3D,0.3D));
-        pointSET.insert(new Point2D(0.4D,0.3D));
-        pointSET.insert(new Point2D(0.9D,0.972D));
+        pointSET.insert(new Point2D(0.2D, 0.3D));
+        pointSET.insert(new Point2D(0.1D, 0.3D));
+        pointSET.insert(new Point2D(0.3D, 0.3D));
+        pointSET.insert(new Point2D(0.4D, 0.3D));
+        pointSET.insert(new Point2D(0.9D, 0.972D));
 
         //pointSET.draw();
 
         RectHV hv = new RectHV(0.3D, 0.5, 0.6, 0.6D);
 
         Iterable<Point2D> range = pointSET.range(hv);
-        for(Point2D point2D: range) {
+        for (Point2D point2D : range) {
             point2D.toString();
         }
 
---
+++ kd-trees-challenge/src/test/java/PointSETTest.java
@@ -0,0 +1,66 @@
+import edu.princeton.cs.algs4.Point2D;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PointSETTest {
+
+    @Test
+    public void nearestTest(){
+        PointSET pointSET = new PointSET();
+        pointSET.insert(new Point2D(1.0D, 1.0D));
+        pointSET.insert(new Point2D(0.0D, 0.25D));
+        pointSET.insert(new Point2D(0.25D, 0.0D));
+        pointSET.insert(new Point2D(1.0D, 0.5D));
+        pointSET.insert(new Point2D(0.75D, 0.0D));
+        pointSET.insert(new Point2D(0.75D, 0.25D));
+        pointSET.insert(new Point2D(0.5D, 0.75D));
+        pointSET.insert(new Point2D(1.0D, 1.0D));
+        pointSET.insert(new Point2D(0.25D, 0.5D));
+        pointSET.insert(new Point2D(1.0D, 0.5D));
+
+        Point2D query = new Point2D(0.5D, 0.75D);
+        Point2D nearest = pointSET.nearest(query);
+
+        Assertions.assertEquals(0.5D, nearest.x());
+        Assertions.assertEquals(0.75D, nearest.y());
+        Assertions.assertEquals(0, query.distanceSquaredTo(nearest));
+    }
+
+    @Test
+    public void shouldThrownException(){
+        PointSET pointSET = new PointSET();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> pointSET.insert(null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> pointSET.contains(null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> pointSET.range(null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> pointSET.nearest(null));
+
+    }
+
+    @DisplayName("Test 3: insert random points; check nearest() with random query points\n" +
+            "  * 10 random points in a 4-by-4 grid")
+    @Test
+    public void caseTest3(){
+        PointSET pointSET = new PointSET();
+        pointSET.insert(new Point2D(1.0D, 1.0D));
+        pointSET.insert(new Point2D(0.25D, 0.75D));
+        pointSET.insert(new Point2D(0.5D, 0.25D));
+        pointSET.insert(new Point2D(0.75D, 0.25D));
+        pointSET.insert(new Point2D(0.75D, 1.0D));
+        pointSET.insert(new Point2D( 0.5D, 1.0D));
+        pointSET.insert(new Point2D(1.0D, 0.0D));
+        pointSET.insert(new Point2D(1.0D, 0.75D));
+        pointSET.insert(new Point2D(0.5D, 0.25D));
+        pointSET.insert(new Point2D(0.25D, 0.25D));
+
+        Point2D queryPoint = new Point2D(0.5D,0.75D);
+        Point2D nearest = pointSET.nearest(queryPoint);
+
+        Assertions.assertEquals(0.25D, nearest.x());
+        Assertions.assertEquals(0.75D, nearest.y());
+
+        Assertions.assertEquals(0.0625D, queryPoint.distanceSquaredTo(nearest));
+    }
+
+}
